ae882322eb338ab1f360d5e1a734cd1931f85ea4
ZedFeorius <zedfeorius7879@gmail.com>
2023-06-27 14:03:02 +0800

    edition of timeout in gateway model

---
+++ miniprogram/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @author ZedFeorius
+ * @version 1.0.0
+ * @date 06-27-2023  14-01-08
+ * @packageName
+ * @className ${NAME}
+ * @describe TODO
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
